3b5d618539a791b597130ef5da4b28bce851d41f
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2024-09-15 00:05:46 +0300

    ops(db): Configure the database connection #1

---
+++ AutoChronos.API/Migrations/20240914205857_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AutoChronos.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Manufacturer = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    CurrentKilometers = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Insurances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CarId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insurances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Insurances_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OilChanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ChangeDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CurrentKilometers = table.Column<int>(type: "integer", nullable: false),
+                    FiltersChanged = table.Column<List<string>>(type: "text[]", nullable: true),
+                    CarId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OilChanges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OilChanges_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Insurances_CarId",
+                table: "Insurances",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OilChanges_CarId",
+                table: "OilChanges",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Insurances");
+
+            migrationBuilder.DropTable(
+                name: "OilChanges");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ AutoChronos.API/Models/OilChange.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoChronos.API.Models
+{
+    public class OilChange
+    {
+        public int Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public int CurrentKilometers { get; set; }
+        public List<string>? FiltersChanged { get; set; }
+        public int CarId { get; set; }
+        public required Car Car { get; set; }
+    }
+
+}
