5ee5135340c77d7e417e64ff5593f89d0ea5bc2b
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2024-10-01 01:20:53 +0300

    feat(authentication): Implement email confirmation for user registration #7

--- AutoChronos.API/Controllers/AuthController.cs
+++ AutoChronos.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoChronos.API.Dtos;
 using AutoChronos.API.Models;
+using AutoChronos.API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +8,7 @@ namespace AutoChronos.API.Controllers
 {
     [ApiController]
     [Route("api/auth")]
-    public class AuthController(UserManager<User> userManager) : ControllerBase
+    public class AuthController(UserManager<User> userManager, IEmailService emailService) : ControllerBase
     {
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterDto model)
@@ -27,6 +28,12 @@ namespace AutoChronos.API.Controllers
 
             if (result.Succeeded)
             {
+
+                var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Auth", new { token, email = user.Email }, Request.Scheme);
+
+                await emailService.SendEmailAsync(user.Email, "Confirm your email", $"Please confirm your account by clicking this link: {confirmationLink}");
+
                 return Ok("User registered successfully.");
             }
 
@@ -37,5 +44,24 @@ namespace AutoChronos.API.Controllers
 
             return BadRequest(ModelState);
         }
+
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return BadRequest("Invalid email address.");
+            }
+
+            var result = await userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return Ok("Email confirmed successfully.");
+            }
+
+            return BadRequest("Email confirmation failed.");
+        }
+
     }
 }
