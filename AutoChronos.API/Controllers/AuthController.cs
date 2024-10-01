9c75e3b2796648a77aac70c73f85f706fd27e51a
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2024-10-01 23:42:41 +0300

    feat(authentication): Implement login endpoint for user authentication #8

--- AutoChronos.API/Controllers/AuthController.cs
+++ AutoChronos.API/Controllers/AuthController.cs
@@ -3,12 +3,17 @@ using AutoChronos.API.Models;
 using AutoChronos.API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 
 namespace AutoChronos.API.Controllers
 {
     [ApiController]
     [Route("api/auth")]
-    public class AuthController(UserManager<User> userManager, IEmailService emailService) : ControllerBase
+    public class AuthController(UserManager<User> userManager, IEmailService emailService, IConfiguration configuration) : ControllerBase
     {
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterDto model)
@@ -63,5 +68,47 @@ namespace AutoChronos.API.Controllers
             return BadRequest("Email confirmation failed.");
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+
+            if (!user.EmailConfirmed)
+            {
+                return Unauthorized("Email not confirmed.");
+            }
+
+            var passwordValid = await userManager.CheckPasswordAsync(user, model.Password);
+            if (!passwordValid)
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+
+            var authClaims = new List<Claim>
+            {
+                new(ClaimTypes.Name, user.UserName ?? string.Empty),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
     }
 }
